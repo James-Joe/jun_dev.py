6561a6ef42ec752ab9b67e56cd026c7dc6f7fecb
James-Joe <jacobassplayer@hotmail.co.uk>
2021-05-07 22:25:41 +0100

    Function created that loops program until user exits

--- odd_or_even.py
+++ odd_or_even.py
@@ -1,13 +1,29 @@
 #A simple program that asks the user for a number and tells the user if that number is odd or even.
 #I hope to build on this program so that it can keep asking the user to enter a number until they choose to exit the program.
 
-intro = "Hello!  Give me a number, and I'll tell you if it is odd or even"
+intro = "Hello!  Give me a number, and I'll tell you if it is odd or even.  Type 0 to exit"
 
 print(intro)
 
-numb = int(input())
 
-if numb % 2 == 0:
-    print(str(numb) + " is an even number!")
-else:
-    print(str(numb) + " is an odd number")
+
+
+def odd_even():
+
+    numb = int(input())
+
+    if numb == 0:
+        print("Bye for now")
+
+    elif numb % 2 == 0:
+        print(str(numb) + " is an even number.  Try again!")
+        odd_even()
+
+    else:
+        print(str(numb) + " is an odd number.  Try again!")
+        odd_even()
+
+
+odd_even()
+        
+
