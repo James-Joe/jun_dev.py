b9cbe27b6b301825ffc85e602aaa68987c59740a
James-Joe <jacobassplayer@hotmail.co.uk>
2021-05-07 12:15:42 +0100

    Basic functionality created

--- odd_or_even.py
+++ odd_or_even.py
@@ -1,4 +1,5 @@
 #A simple program that asks the user for a number and tells the user if that number is odd or even.
+#I hope to build on this program so that it can keep asking the user to enter a number until they choose to exit the program.
 
 intro = "Hello!  Give me a number, and I'll tell you if it is odd or even"
 
