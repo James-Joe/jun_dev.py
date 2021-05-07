e9c6120035f36774cd4c077bb8eef1962502aff7
James-Joe <jacobassplayer@hotmail.co.uk>
2021-05-07 22:28:44 +0100

    Comments added, printed text modified.

--- odd_or_even.py
+++ odd_or_even.py
@@ -1,5 +1,6 @@
-#A simple program that asks the user for a number and tells the user if that number is odd or even.
-#I hope to build on this program so that it can keep asking the user to enter a number until they choose to exit the program.
+# A simple program that asks the user for a number and tells the user if that number is odd or even.
+# I created a function that takes the user's input, returns an answer and prompts them to try again.
+# If they type 0, the program terminates.
 
 intro = "Hello!  Give me a number, and I'll tell you if it is odd or even.  Type 0 to exit"
 
@@ -16,11 +17,11 @@ def odd_even():
         print("Bye for now")
 
     elif numb % 2 == 0:
-        print(str(numb) + " is an even number.  Try again!")
+        print(str(numb) + " is an even number.  Try again! Or type 0 to exit.")
         odd_even()
 
     else:
-        print(str(numb) + " is an odd number.  Try again!")
+        print(str(numb) + " is an odd number.  Try again! Or type 0 to exit")
         odd_even()
 
 
