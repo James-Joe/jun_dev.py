ea844e04ea369474636ae792e3f63def915b8a00
James-Joe <jacobassplayer@hotmail.co.uk>
2021-05-15 22:16:36 +0100

    Function does not count non-alpha-numeric characters

--- word_count.py
+++ word_count.py
@@ -1,8 +1,12 @@
 #This program takes a text input and returns the word_count
 
-some_input = "haec uestis priscis hominum uariata figuris"
+some_input = "haec uestis priscis | hominum uariata figuris"
  
 make_list = some_input.split()
+
+for i in make_list:
+    if i.isalnum() == False:
+        make_list.remove(i)
 
 length_lst= len(make_list)
 
