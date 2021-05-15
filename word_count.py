38431cb880e7cbec7ced716874107c5b352b8233
James-Joe <jacobassplayer@hotmail.co.uk>
2021-05-15 23:08:18 +0100

    word_count takes file, opens it, reads it, returns word count; more still to do

--- word_count.py
+++ word_count.py
@@ -1,12 +1,17 @@
 #This program takes a text input and returns the word_count
 
-some_input = "haec uestis priscis | hominum uariata figuris"
+some_input = open("eliot.txt")
  
-make_list = some_input.split()
+make_list = some_input.read().split()
 
-for i in make_list:
-    if i.isalnum() == False:
-        make_list.remove(i)
+#the loop below doesn't do what I want to yet - but my results are just as good as libreoffice at the moment
+
+for word in make_list:
+    for character in word:
+        if character.isalnum() == False:
+            word.replace(character, " ")
+
+print(make_list)
 
 length_lst= len(make_list)
 
