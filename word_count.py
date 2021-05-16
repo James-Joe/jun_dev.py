d82655e37b9f07200b3ae6fc2a6e4999d203a6b7
James-Joe <jacobassplayer@hotmail.co.uk>
2021-05-16 21:37:05 +0100

    Comments added; word_count() function completed; functionality to prompt user for another text

--- word_count.py
+++ word_count.py
@@ -1,13 +1,46 @@
-#This program takes a text input and returns the word_count
+#This program takes a text input and returns the word_count.
+# 
+# word_count takes a filename as input, opens it, reads it, then turns the input
+# into a list (make_list) separated by whitespace using .split().
+# A new_list is then created that takes out any punctuation that may be counted 
+# as a word - such as an elipsis ("...").  
 
-some_input = open("eliot.txt")
- 
-make_list = some_input.read().split()
+# prompt_continue() then asks the user if they want to count something 
+# else or to quit the program.
 
-new_list = [word for word in make_list if word.startswith((".", ",", ";", ":", "?", "!")) == False]
 
-print(new_list)
 
-length_lst= len(new_list)
 
-print("Your input has " + str(length_lst) + " words")
+def word_count():
+    print("Provide the file name of the text you would like us to word count")
+    some_input = open(input())
+    
+    make_list = some_input.read().split()
+
+    new_list = [word for word in make_list if word.startswith((".", ",", ";", ":", "?", "!", "-")) == False]
+
+    print(new_list)
+
+    length_lst= len(new_list)
+
+    print("Your text has " + str(length_lst) + " words.")
+
+    prompt_continue()
+
+    
+def prompt_continue():
+
+    print("Would you like us to read another text? [Y/N]")
+    restart = input()
+
+    if restart == "Y" or restart == "y":
+        word_count()
+    elif restart == "N" or restart == "n":
+        print("Goodbye!")
+    else:
+        print("Please enter a valid input [Y/N]")
+        prompt_continue()
+
+
+
+word_count()
