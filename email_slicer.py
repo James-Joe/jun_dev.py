e7be95194bd114c27f40a8e39ce3ad637dfd7963
James-Joe <jacobassplayer@hotmail.co.uk>
2021-07-12 14:19:51 +0100

    Code tidied up; comments added

--- email_slicer.py
+++ email_slicer.py
@@ -1,29 +1,24 @@
-email = "jacobassplayer@hotmail.co.uk"
-
-
-# Method 1
-
-# get_username = email.split("@")
-
-# username = get_username[0]
+#In this project, I attempt to obtain the domain name of an email from user input.  I then take this
+# information and send an email to the address that has been inputted.
 
 
 
-# Method 2
 
-# name = ""
 
-# for i in email:
-  #  if i == "@":
-   #     break
-   # name += i
-
-# print(name)
-
-# Method 3
-
-def method_3(email):
+def get_domain(email):
     name = email[0:email.index("@")]
-    print(name)
+    domain = email[email.index("@") + 1:]
+    spliced_domain = domain.split(".")
 
-method_3(email)
+    if spliced_domain[0] == "gmail":
+      print("Well, " + name + " looks like you have a gmail account")
+    elif spliced_domain[0] == "hotmail":
+      print("Well, " + name + " looks like you have a hotmail account")
+    else:
+      print("Ohh!  Looks like you have a custom domain name ... Noice!")
+
+
+print("Hey! What's your email?")
+email = input()
+
+get_domain(email)
