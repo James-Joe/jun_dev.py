c9238ebd554259c2e9cf7d4fbe2f34ee16400281
James-Joe <jacobassplayer@hotmail.co.uk>
2021-08-29 17:21:14 +0100

    simplified to one function that works

--- palindrome.py
+++ palindrome.py
@@ -1,17 +1,11 @@
 #In this programme, I take an input of 5 words from the user, and check if any of those words are a palindrome.
 
-def get_input():
+def is_palindrome():
     words = input("Please input 5 words or phrases separated by a comma\n")
 
-    lst = words.split(",")
+    lst = words.lower().split(", ")
+    print(lst)
 
-    return lst
-
-
-
-def reverse_list(lst):
-
-#    rev_lst = [i[::-1] for i in lst]
     for i in lst:
         if i != i[::-1]:
             continue
@@ -19,10 +13,4 @@ def reverse_list(lst):
     
 
 
-
-wrds = ["radar", "steve", "mum"]
-
-for word in wrds:
-    if word != word[::-1]:
-        continue
-    print(word + " is a palindrome")
+is_palindrome()
